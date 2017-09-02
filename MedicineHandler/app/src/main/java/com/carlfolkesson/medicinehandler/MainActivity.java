c6e0a3cc1268bdeb113d1407bd0d312a05b81d1c
carlFolkesson <carfo452@student.liu.se>
2017-09-02 21:11:38 +0200

    Add feature to save the medicine list when app exits and to load it when i starts

--- MedicineHandler/app/src/main/java/com/carlfolkesson/medicinehandler/MainActivity.java
+++ MedicineHandler/app/src/main/java/com/carlfolkesson/medicinehandler/MainActivity.java
@@ -1,17 +1,22 @@
 package com.carlfolkesson.medicinehandler;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.text.format.DateFormat;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     private List<MedicineItem> medicineItems;
 
-    private int index = 0;
+    private int medicineListIndex = 0;
+
+    // SharedPreferences keys
+    final private String medicineListIndexKey = "com.carlfolkesson.medicinehandler.medicineListIndex";
+    final private String medicineItemsKey = "com.carlfolkesson.medicinehandler.medicineItems";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,16 +58,18 @@ public class MainActivity extends AppCompatActivity {
         fabRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(index > 0) {
-                    medicineItems.remove(index-1);
-                    index--;
+                if(medicineListIndex > 0) {
+                    medicineItems.remove(medicineListIndex -1);
+                    medicineListIndex--;
                     adapter.notifyDataSetChanged();
                 }
             }
         });
 
         setDate();
+        medicineItems = new ArrayList<>();
         createRecyclerView();
+        loadSavedData();
     }
 
     @Override
@@ -83,25 +94,39 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setDate() {
+        final Calendar t = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("EEEE yyyy-MM-dd");
+        String currentDateTimeString = df.format(t.getTime());
+        TextView date = (TextView) findViewById(R.id.date);
+        date.setText(currentDateTimeString);
+    }
+
     /**
-     * Creates a RecyclerView containing a name and a number
+     * Creates a RecyclerView containing name, number of items in stock and daily dose of a medicine
      */
     private void createRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        medicineItems = new ArrayList<>();
-        adapter = new MedicineAdapter(medicineItems);
-        recyclerView.setAdapter(adapter);
     }
 
-    private void setDate() {
-        final Calendar t = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("EEEE yyyy-MM-dd");
-        String currentDateTimeString = df.format(t.getTime());
-        final TextView date = (TextView) findViewById(R.id.date);
-        date.setText(currentDateTimeString);
+    private void loadSavedData() {
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        // Loads in the saved value of medicineListIndex
+        medicineListIndex = sharedPref.getInt(medicineListIndexKey, 0);
+
+        // Loads in the saved value of medicineItems
+        Gson gson = new Gson();
+        String jsonMedicineItems = sharedPref.getString(medicineItemsKey, null);
+        if(jsonMedicineItems != null && !jsonMedicineItems.equals("null")) {
+            Log.i("Inte Null", jsonMedicineItems);
+            medicineItems = gson.fromJson(jsonMedicineItems, new TypeToken<List<MedicineItem>>() {}.getType());
+        }
+        adapter = new MedicineAdapter(medicineItems);
+        recyclerView.setAdapter(adapter);
     }
 
     /**
@@ -113,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // If an new medicin was added
+        // If an new medicine was added
         if (requestCode == 1) {
             if(resultCode == RESULT_OK) {
                 String medicineName = data.getStringExtra("name");
@@ -124,9 +149,26 @@ public class MainActivity extends AppCompatActivity {
                         medicineStock,
                         medicineDose);
                 medicineItems.add(medicineItem);
-                index++;
+                medicineListIndex++;
                 adapter.notifyDataSetChanged();
             }
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // Saves the current medicineListIndex to the phone
+        editor.putInt(medicineListIndexKey, medicineListIndex);
+
+        // Saves the current medicineItems to the phone
+        Gson gson = new Gson();
+        String jsonMedicineItems = gson.toJson(medicineItems);
+        editor.putString(medicineItemsKey, jsonMedicineItems);
+
+        editor.apply();
     }
 }
